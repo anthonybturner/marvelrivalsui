d8f8bc8c5892e8725c3676f5ab2500263a5bd2f8
Anthony <anthonybturner@icloud.com>
2025-08-29 07:48:52 -0400

    updated api urls

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://marvelrivalsapi.onrender.com/api/',
+  apiUrl: 'https://marvelrivalsapi-production.up.railway.app/api/',
   remoteApiUrl: 'https://marvelrivalsapi.com/api/' // remote API URL
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:44312/api/', // local .NET API URL
+  apiUrl: 'https://marvelrivalsapi-production.up.railway.app/api/', // local .NET API URL
   remoteApiUrl: 'https://marvelrivalsapi.com/api/' // remote API URL
 };
