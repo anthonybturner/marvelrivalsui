6c836a1094d1bfd65064078797ce79288fc161a2
Anthony <anthonybturner@icloud.com>
2025-10-27 00:14:51 -0400

    removed dummy client secret and console logs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ export const environment = {
   apiUrl: 'https://marvelrivalsapi-production.up.railway.app/api/',
   remoteApiUrl: 'https://marvelrivalsapi.com/api/', // remote API URL
   googleClientId: '178434119476-ne91j0g3looqspstf1b7v1pk1tqej2j8.apps.googleusercontent.com',
+  devMode: false,
 };
