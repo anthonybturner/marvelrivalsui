40905351d9dc1c23078de066875c75f340788d27
Anthony <anthonybturner@icloud.com>
2025-10-27 01:58:15 -0400

    Remove environment files from repository

--- src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://marvelrivalsapi-production.up.railway.app/api/', // local .NET API URL
-  remoteApiUrl: 'https://marvelrivalsapi.com/api/', // remote API URL
-  googleClientId: '178434119476-ne91j0g3looqspstf1b7v1pk1tqej2j8.apps.googleusercontent.com',
-  devMode: true,
-};
